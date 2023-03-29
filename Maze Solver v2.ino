474581cb0fc655c718134f9cd15a9ff25e348976
yunusgunok123 <91676137+yunusgunok123@users.noreply.github.com>
2023-03-29 20:26:28 +0300

    Maze Sover Commented v1.1

--- Maze Solver v2.ino
+++ Maze Solver v2.ino
@@ -1,13 +1,23 @@
+// Include the library header file
 #include "./lib.h"
 
-// Kodu kontrol ederken fark ettim mikrokontroller çok büyük array kullanamıyor
-// Yani labirentin ölçüleri mak 31 x 31 olmalı
-Solver<20> solver;
+// Create an instance of the Solver class with a maximum size of 31
+Solver<31> solver;
+
+// Setup function that sets pin 13 as input with pullup resistor
 void setup() {
-  Serial.begin(9600);
+  pinMode(13, INPUT_PULLUP);
 }
 
+// Loop function that checks if button connected to pin 13 is pressed
+// If it is, the robot checks the directions it can move in and moves in the second direction in the array
+// The first direction is always backwards since it is always possible to move backwards
 void loop() {
-  Dir dir = left;
-  solver.move(dir);
+  if (digitalRead(13)){
+    delay(1000);
+    Dir dirs[4];
+    unsigned char size = 0;
+    solver.checkDir(dirs, size);
+    solver.move(dirs[1]);
+  }
 }
